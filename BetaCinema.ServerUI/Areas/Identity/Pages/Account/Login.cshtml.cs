983fbc88215ee6bdba18e88ac77112f16c81c437
The Anh <anhtheit2002@gmail.com>
2023-12-29 15:22:49 +0700

    feat: export excel, import excel, add tab movies, fix google auth error overflow password

--- BetaCinema.ServerUI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BetaCinema.ServerUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -42,7 +42,7 @@ namespace BetaCinema.ServerUI.Areas.Identity.Pages.Account
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            ExternalLogins = (await SignInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
         public async Task<IActionResult> OnPostAsync()
